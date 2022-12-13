0328ce55c0bbdb6c9712901b1b923d996682454f
jowc <jchikeme@gmail.com>
2022-12-13 17:47:18 +0100

    added function to load modal

--- js/script.js
+++ js/script.js
@@ -19,3 +19,28 @@
     } else for (n = 0; n < l.length; n++) l[n].className = "menu";
   }
 })();
+
+let modalStates = document.querySelectorAll(".modal-state");
+let modalExpand = document.getElementById("modal-expanded");
+
+function openModal() {
+  replaceClass(modalStates);
+}
+
+function closeModal() {
+  for (var i = 0; i < modalStates.length; i++) {
+    modalStates[i].classList.remove("hidden");
+  }
+  modalExpand.classList.add("hidden");
+  modalExpand.classList.remove("block");
+}
+
+function replaceClass(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle("hidden");
+    if (modalExpand.classList.contains("hidden")) {
+      modalExpand.classList.remove("hidden");
+      modalExpand.classList.add("block");
+    }
+  }
+}
