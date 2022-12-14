6f0093d1c36171cf6b34b3e8e365f47d51e69c7e
jowc <jchikeme@gmail.com>
2022-12-14 13:26:24 +0100

    added close button to drawer and open modal on page load

--- js/script.js
+++ js/script.js
@@ -20,27 +20,43 @@
   }
 })();
 
+// declare DOM elements
+let drawer = document.getElementById("drawer");
 let modalStates = document.querySelectorAll(".modal-state");
 let modalExpand = document.getElementById("modal-expanded");
 
-function openModal() {
-  replaceClass(modalStates);
+//auto load modal
+window.onload = setTimeout(() => {
+  openDrawer();
+}, 3000);
+
+//Open drawer
+function openDrawer() {
+  drawer.classList.toggle("hidden");
 }
 
+//close drawer
+function closeDrawer() {
+  let drawer = document.getElementById("drawer");
+  return drawer.classList.toggle("hidden");
+}
+
+// open carousel modal
+function openModal() {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle("hidden");
+    if (modalExpand.classList.contains("hidden")) {
+      modalExpand.classList.remove("hidden");
+      modalExpand.classList.add("grid");
+    }
+  }
+}
+
+//close carousel modal
 function closeModal() {
   for (var i = 0; i < modalStates.length; i++) {
     modalStates[i].classList.remove("hidden");
   }
   modalExpand.classList.add("hidden");
-  modalExpand.classList.remove("block");
-}
-
-function replaceClass(elements) {
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.toggle("hidden");
-    if (modalExpand.classList.contains("hidden")) {
-      modalExpand.classList.remove("hidden");
-      modalExpand.classList.add("block");
-    }
-  }
+  modalExpand.classList.remove("grid");
 }
